78e8855e1673b74836945c9a7c1f9b35a6f53cb2
henrietta22589 <93531231+henrietta22589@users.noreply.github.com>
2022-03-30 23:30:20 +0300

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp.ShortingAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDatabase db = new MyDatabase();
+            var shirts = db.TShirts;
+
+
+
+            // shirts.BubbleOrderBySize();
+            //shirts.BubbleOrderBySizeColorFabric();
+            //QuickShort.quickSortOrderByColor(shirts, 0, shirts.Count - 1);
+
+            //shirts.quickSortOrderByColor(0, shirts.Count - 1);
+            //shirts.quickSortOrderBySize(0, shirts.Count - 1);
+            //var shirts = db.TShirts.OrderBy(x=>x.Fabric).ThenBy(x=>x.Color).ThenBy(x=>x.Size);
+            //////shirts.BubbleOrderByColor();
+            //shirts.BurbbleOrderBySizeColorFabric();
+
+
+            //shirts.BucketSortByColor();
+
+            foreach (var shirt in shirts)
+            {
+                shirt.Output();
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Tshirt.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp.Enum;
+
+public class TShirt
+{
+    public Color Color;
+    public Size Size;
+    public Fabric Fabric;
+    public decimal Price { get; set; }
+    public TShirt(Color color, Size size, Fabric fabric)
+    {
+        Color = color;
+        Size = size;
+        Fabric = fabric;
+    }
+    public void Output()
+    {
+        System.Console.WriteLine($"{Size,-15} {Color,-15} {Fabric,-15}");
+    }
+}
