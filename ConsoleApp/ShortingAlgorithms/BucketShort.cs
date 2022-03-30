1fb2f826ca083ffcf58830ee784bad415801770a
henrietta22589 <93531231+henrietta22589@users.noreply.github.com>
2022-03-30 23:30:43 +0300

    Add files via upload

---
+++ ConsoleApp/ShortingAlgorithms/BucketShort.cs
@@ -0,0 +1,80 @@
+﻿using ConsoleApp.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.ShortingAlgorithms
+{
+    public static class BucketShort
+    {
+
+
+        public static List<TShirt> BucketSortByColor(this List<TShirt> shirts)
+        {
+
+            List<List<TShirt>> buckets = new List<List<TShirt>>();
+            InitializeBuckets(buckets);
+            Scatter(shirts, buckets);
+            int i = 0;
+            foreach (List<TShirt> bucket in buckets)
+            {
+                List<TShirt> shirt = bucket.ToList();
+                InsertionSort(shirt);
+                foreach (TShirt d in shirt)
+                {
+                    shirts[i++] = d;
+                }
+            }
+            return shirts;
+        }
+        //Προσθετει το μπλουζακι στην αντιστοιχη θεση με το χρωμα του!
+        private static void Scatter(this List<TShirt> shirts, List<List<TShirt>> buckets)
+        {
+            foreach (TShirt value in shirts)
+            {
+                int bucketNumber = GetBucketNumber(value);
+                buckets[bucketNumber].Add(value);
+            }
+        }
+        //Ταξινομηση με βαση το χρωμα!
+        private static void InsertionSort(this List<TShirt> shirts)
+        {
+            int j;
+            TShirt temp;
+            for (int i = 1; i < shirts.Count; i++)
+            {
+                j = i;
+                while (j > 0 && shirts[j].Color < shirts[j - 1].Color)
+                {
+                    temp = shirts[j];
+                    shirts[j] = shirts[j - 1];
+                    shirts[j - 1] = temp;
+                    j--;
+                }
+            }
+        }
+        private static int GetBucketNumber(TShirt value)
+        {
+            Color val = value.Color;
+            int bucketNumber = (int)val;
+
+            return bucketNumber;
+
+
+
+        }
+        //Φτιαχνει "Kαλαθι" με κενες Λιστες οσο το πληθος των χρωματων Tshirt !!
+        private static void InitializeBuckets(List<List<TShirt>> buckets)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                List<TShirt> a = new List<TShirt>();
+                buckets.Add(a);
+            }
+        }
+        
+    }
+
+}
---
+++ ConsoleApp/ShortingAlgorithms/QuickShort.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.ShortingAlgorithms
+{
+    public static class QuickShort
+    {
+    public    static void quickSortOrderByColor(this List<TShirt> shirts, int low, int high)
+        {
+            if (low < high)
+            {
+                /* pi is partitioning index, arr[pi] is  
+                now at right place */
+                int pi = partitionOrderByColor(shirts, low, high);
+                // Recursively sort elements before 
+                // partition and after partition 
+                quickSortOrderByColor(shirts, low, pi - 1);
+                quickSortOrderByColor(shirts, pi + 1, high);
+            }
+        }
+       public static int partitionOrderByColor(this List<TShirt> shirts, int low, int high)
+        {
+            TShirt pivot = shirts[high];
+            // index of smaller element 
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                // If current element is smaller  
+                // than the pivot 
+                if (shirts[j].Color < pivot.Color)
+                {
+                    i++;
+                    // swap arr[i] and arr[j] 
+                    TShirt temp = shirts[i];
+                    shirts[i] = shirts[j];
+                    shirts[j] = temp;
+                }
+            }
+            // swap arr[i+1] and arr[high] (or pivot) 
+            TShirt temp1 = shirts[i + 1];
+            shirts[i + 1] = shirts[high];
+            shirts[high] = temp1;
+            return i + 1;
+        }
+
+        public static void quickSortOrderBySize(this List<TShirt> shirts, int low, int high)
+        {
+            if (low < high)
+            {
+                /* pi is partitioning index, arr[pi] is  
+                now at right place */
+                int pi = partitionOrderBySize(shirts, low, high);
+                // Recursively sort elements before 
+                // partition and after partition 
+                quickSortOrderBySize(shirts, low, pi - 1);
+                quickSortOrderBySize(shirts, pi + 1, high);
+            }
+        }
+
+        public static int partitionOrderBySize(this List<TShirt> shirts, int low, int high)
+        {
+            TShirt pivot = shirts[high];
+            // index of smaller element 
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                // If current element is smaller  
+                // than the pivot 
+                if (shirts[j].Size < pivot.Size)
+                {
+                    i++;
+                    // swap arr[i] and arr[j] 
+                    TShirt temp = shirts[i];
+                    shirts[i] = shirts[j];
+                    shirts[j] = temp;
+                }
+            }
+            // swap arr[i+1] and arr[high] (or pivot) 
+            TShirt temp1 = shirts[i + 1];
+            shirts[i + 1] = shirts[high];
+            shirts[high] = temp1;
+            return i + 1;
+        }
+    }
+}
