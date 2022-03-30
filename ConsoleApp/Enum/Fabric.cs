e150d22cef7b23b8f0f47c52e04e17ed468e50b7
henrietta22589 <93531231+henrietta22589@users.noreply.github.com>
2022-03-30 23:29:48 +0300

    Add files via upload

---
+++ ConsoleApp/Enum/Fabric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Enum
+{
+    public enum Fabric
+    {
+        CASHMERE,
+        COTTON,
+        LINEN,
+        POLYESTER,
+        RAYON,
+        SILK,
+        WOOL
+    }
+
+
+
+
+
+
+}
---
+++ ConsoleApp/Enum/Size.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Enum
+{
+    public enum Size
+    {
+        XS,
+        S,
+        M,
+        L,
+        XL,
+        XXL,
+        XXXL
+    }
+
+
+}
